f352622a5e7206549624a759460a395efe985191
Keith Potempa <keithrpotempa@gmail.com>
2020-01-27 16:12:17 -0600

    parks api manager complete using the q method

---
+++ src/scripts/apiManager/park-api-manager.js
@@ -0,0 +1,17 @@
+const apiBaseUrl = `https://data.nashville.gov/resource/74d7-b74t.json?`
+
+const searchCriteria =  "Metro"
+
+//Searches Restaurants 
+const parksApiManager = {
+  searchRestaurants(searchCriteria) {
+    const url = apiBaseUrl + `$q=${searchCriteria}`
+    return fetch(url).then(resp => resp.json());
+  }
+};
+
+const parksSearchResultPromise = parksApiManager.searchRestaurants(searchCriteria);
+
+// parksSearchResultPromise.then(searchResults => {
+//     console.log(searchResults);
+// });
