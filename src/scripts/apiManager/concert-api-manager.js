021ff5f49a9ff53ff5b17510387c6810f375c5a0
Matt Reeds <mattlreeds@gmail.com>
2020-01-28 10:46:33 -0600

    functioning fetch call

--- src/scripts/apiManager/concert-api-manager.js
+++ src/scripts/apiManager/concert-api-manager.js
@@ -1,10 +1,12 @@
     const apiConcertUrl = "https://app.ticketmaster.com/discovery/v2/events.json?";
     
-    const apiManager = {
+    const apiConcertManager = {
         searchConcerts(searchCriteria) {
-            const criteria = encodeURIComponent(`"%${searchCriteria}%"`);
-            const url = apiConcertUrl + `$where=type like ${criteria}` + `apikey=${apiKeys.TicketMaster}`;
+            const url = apiConcertUrl + `$classificationName=music&marketId=31` + 
+            `&keyword=${searchCriteria}` + 
+            `&apikey=${apiKeys.TicketMaster}`;
+            
             return fetch(url).then(resp => resp.json());
         }
     }; 
-    console.log(apiManager.searchConcerts("event"))
+    // console.log(apiConcertManager.searchConcerts("top"))
