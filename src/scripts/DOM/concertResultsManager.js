c645d56786fa525d9f95a3237b9e7a5affce5dd8
Keith Potempa <keithrpotempa@gmail.com>
2020-01-28 15:38:30 -0600

    finished the concerts dom manager

--- src/scripts/DOM/concertResultsManager.js
+++ src/scripts/DOM/concertResultsManager.js
@@ -1,34 +1,60 @@
 let concert_id = 0;
 
-const concertFactory = (searchCriteria) => {
-    concert_id ++;
-    const entryField = document.getElementById("search-concerts");
-    const outputContainer = document.getElementById("itineraryContainer");
-    const card = document.createElement("article");
-    // card.classList.add("border");
-    card.id = `card--${concert_id}`;
-    // Change this
-    card.textContent = searchResults;
+const concertSearchResultsDomManager = {
+    concertFactory(concert){
+        concert_id ++;
+        return `
+            <section class="concert" id="concert--${concert_id}">
+                <br>
+                <div class="concert-name">${concert.name}</div>
+                <div class="concert-date">${concert.dates.start.localDate}</div>
+                <div class="concert-time">${concert.dates.start.localTime}</div>
+                <div class="concert-links"><a href="${concert.url}">Link</a></div>
+                <button id="save--${concert_id}" class="concert-save">
+                    Save &#11088;
+                </button>
+      
+                <br>
+            </section>
+        `
+    },
+    renderSearchResults(searchResults) {
+        const concerts = searchResults._embedded.events
+        console.log(concerts);
+        const container = document.querySelector("#resultsContainer");
+        container.innerHTML = "<h2>Concert Results</h2>";
+        concerts.forEach(concert => {
+          container.innerHTML += this.concertFactory(concert);
+        });
 
-    const saveButton = document.createElement("BUTTON")
-    saveButton.innerHTML = "SAVE";
-    saveButton.id = `save--${concert_id}`;
-    saveButton.classList.add("saveButton");
-    saveButton.addEventListener("click", saveCard);
-    card.appendChild(saveButton);
+    }
 
-    outputContainer.appendChild(card);
 }
 
-const saveCard = (event) => {
-    const id = event.target.id.split("-")[2];
-    const card = document.getElementById(`card--${id}`)
-    outputContainer.removeChild(card)
-}
 
-// Delete or comment this after testing
-const searchCriteria = "ZZ Top";
-const searchResultPromise = apiConcertManager.searchConcerts(searchCriteria);
-searchResultPromise.then(searchResults => {
-  concertFactory(searchResults);
-});
+// Half-finished code to save:
+// This should go on the event listener...
+// const saveCard = (event) => {
+//     const id = event.target.id.split("-")[2];
+//     const card = document.getElementById(`card--${id}`)
+//     outputContainer.removeChild(card)
+// }
+
+// Abandoned method
+// concert_id ++;
+// const entryField = document.getElementById("search-concerts");
+// const outputContainer = document.getElementById("itineraryContainer");
+// const card = document.createElement("article");
+// // card.classList.add("border");
+// card.id = `card--${concert_id}`;
+// // Change this
+// card.textContent = searchCriteria;
+
+// const saveButton = document.createElement("BUTTON")
+// saveButton.innerHTML = "SAVE";
+// saveButton.id = `save--${concert_id}`;
+// saveButton.classList.add("saveButton");
+// saveButton.addEventListener("click", saveCard);
+// card.appendChild(saveButton);
+
+// outputContainer.appendChild(card);
