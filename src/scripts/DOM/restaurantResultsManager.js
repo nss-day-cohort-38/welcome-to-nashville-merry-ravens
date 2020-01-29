437f75613ac1aaeb95669377eb61571680887f5e
Matt Reeds <mattlreeds@gmail.com>
2020-01-29 13:13:05 -0600

    in progress itinerary button

--- src/scripts/DOM/restaurantResultsManager.js
+++ src/scripts/DOM/restaurantResultsManager.js
@@ -16,7 +16,37 @@ const restaurantsResultsDomManager = {
             const restaurants = searchResults.restaurants
             const container = document.querySelector("#resultsContainer");
             container.innerHTML = "<h2>Restaurant Results</h2>"
+            if(restaurants.length >= 1) {
             restaurants.forEach(restaurant => {
                 container.innerHTML += this.restaurantFactory(restaurant);
-            })
-        }}
+            })} else {
+                container.innerHTML += "<p>No results available</p>"
+            }
+            this.listenerSaveItinerary(`save--${restaurant_id}`);
+        },
+        
+    },
+{
+        const button = document.getElementById(buttonId);
+        button.addEventListener("click", () => {
+            const sectionId = buttonId.split("-")[2]
+            console.log(sectionId)
+        })
+    }
+//     listenerSaveItinerary(buttonId){
+//         const button = document.getElementById(buttonId);
+//         button.addEventListener("click", () => {
+//             const sectionId = buttonId.split("-")[2]; // This grabs the concertId from the button
+//             // console.log(sectionId); 
+//             this.saveItinerary(sectionId);
+            
+//         })
+//     },
+//     saveItinerary(sectionId){
+//         const container = document.getElementById("concertsItineraryContainer");
+//         // console.log(container)
+//         const section = document.getElementById(`concert--${sectionId}`);
+//         // console.log(section);
+//         container.innerHTML = `<h3>Favorite Restaurant:</h2>` + section.innerHTML;
+//     }
+// }
